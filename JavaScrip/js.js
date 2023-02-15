38f0bcb1e88233966a3b623abf066d0b4bad71e1
Diego26194 <ianchinadiego@gmail.com>
2023-02-15 01:02:13 -0300

    first commit

---
+++ JavaScrip/js.js
@@ -0,0 +1,157 @@
+
+const Ningm = document.getElementById('Ningm');
+const Dingm = document.getElementById('Dingm');
+
+const Ningi = document.getElementById('Ningi');
+const Dingi = document.getElementById('Dingi');
+
+const NJava = document.getElementById('NJava');
+const DJava = document.getElementById('DJava');
+
+const NJavaScript = document.getElementById('NJavaScript');
+const DJavaScript = document.getElementById('DJavaScript');
+
+const NHTML = document.getElementById("NHTML");
+const DHTML = document.getElementById("DHTML");
+
+const NCSS = document.getElementById('NCSS');
+const DCSS = document.getElementById('DCSS');
+
+const NRuby = document.getElementById('NRuby');
+const DRuby = document.getElementById('DRuby');
+
+const NC = document.getElementById('NC');
+const DC = document.getElementById('DC');
+
+const NMatlab = document.getElementById('NMatlab');
+const DMatlab = document.getElementById('DMatlab');
+
+const NMySQL = document.getElementById('NMySQL');
+const DMySQL = document.getElementById('DMySQL');
+
+
+Ningm.addEventListener("mouseover", function() {
+  Dingm.style.display = "inline";
+  Dingm.style.display = "table";
+});
+Dingm.addEventListener("mouseover", function() {
+  Dingm.style.display = "inline";
+  Dingm.style.display = "table";
+});
+Ningm.addEventListener("mouseout", function() {
+  Dingm.style.display = "none";
+});
+Dingm.addEventListener("mouseout", function() {
+  Dingm.style.display = "none";
+});
+
+Ningi.addEventListener("mouseover", function() {
+  Dingi.style.display = "inline";
+  Dingi.style.display = "table";
+});
+Dingi.addEventListener("mouseover", function() {
+  Dingi.style.display = "inline";
+  Dingi.style.display = "table";
+});
+Ningi.addEventListener("mouseout", function() {
+  Dingi.style.display = "none";
+});
+Dingi.addEventListener("mouseout", function() {
+  Dingi.style.display = "none";
+});
+
+NJava.addEventListener("mouseover", function() {
+  DJava.style.display = "inline";
+  DJava.style.display = "table";
+});
+DJava.addEventListener("mouseover", function() {
+  DJava.style.display = "inline";
+  DJava.style.display = "table";
+});
+NJava.addEventListener("mouseout", function() {
+  DJava.style.display = "none";
+});
+
+NJavaScript.addEventListener("mouseover", function() {
+  DJavaScript.style.display = "inline";
+  DJavaScript.style.display = "table";
+});
+DJavaScript.addEventListener("mouseover", function() {
+  DJavaScript.style.display = "inline";
+  DJavaScript.style.display = "table";
+});
+NJavaScript.addEventListener("mouseout", function() {
+  DJavaScript.style.display = "none";
+});
+
+NHTML.addEventListener("mouseover", function() {
+  DHTML.style.display = "inline";
+  DHTML.style.display = "table";
+});
+DHTML.addEventListener("mouseover", function() {
+  DHTML.style.display = "inline";
+  DHTML.style.display = "table";
+});
+NHTML.addEventListener("mouseout", function() {
+  DHTML.style.display = "none";
+});
+
+NCSS.addEventListener("mouseover", function() {
+  DCSS.style.display = "inline";
+  DCSS.style.display = "table";
+});
+DCSS.addEventListener("mouseover", function() {
+  DCSS.style.display = "inline";
+  DCSS.style.display = "table";
+});
+NCSS.addEventListener("mouseout", function() {
+  DCSS.style.display = "none";
+});
+
+NRuby.addEventListener("mouseover", function() {
+  DRuby.style.display = "inline";
+  DRuby.style.display = "table";
+});
+DRuby.addEventListener("mouseover", function() {
+  DRuby.style.display = "inline";
+  DRuby.style.display = "table";
+});
+NRuby.addEventListener("mouseout", function() {
+  DRuby.style.display = "none";
+});
+
+NC.addEventListener("mouseover", function() {
+  DC.style.display = "inline";
+  DC.style.display = "table";
+});
+DC.addEventListener("mouseover", function() {
+  DC.style.display = "inline";
+  DC.style.display = "table";
+});
+NC.addEventListener("mouseout", function() {
+  DC.style.display = "none";
+});
+
+NMatlab.addEventListener("mouseover", function() {
+  DMatlab.style.display = "inline";
+  DMatlab.style.display = "table";
+});
+DMatlab.addEventListener("mouseover", function() {
+  DMatlab.style.display = "inline";
+  DMatlab.style.display = "table";
+});
+NMatlab.addEventListener("mouseout", function() {
+  DMatlab.style.display = "none";
+});
+
+NMySQL.addEventListener("mouseover", function() {
+  DMySQL.style.display = "inline";
+  DMySQL.style.display = "table";
+});
+DMySQL.addEventListener("mouseover", function() {
+  DMySQL.style.display = "inline";
+  DMySQL.style.display = "table";
+});
+NMySQL.addEventListener("mouseout", function() {
+  DMySQL.style.display = "none";
+});
